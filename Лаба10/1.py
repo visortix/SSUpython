a3ac4c8cbf699426b036c9f29eb481507a6f909e
Milanovych Dmytro <dimon.lipova@gmail.com>
2024-04-21 19:57:04 +0300

    Added data to 10 lab and 13 lab

--- Лаба10/1.py
+++ Лаба10/1.py
@@ -1,15 +1,41 @@
+def write_data(filename, stud_name, answer, question, flag):
+    try:
+        # Відкриття файлу для допису (створення, якщо не існує)
+        with open(filename, "a", encoding="utf-8") as file:  # Змінено режим на "a"
+            # Запис прізвища та питання у файл
+            file.write(f"--- {stud_name} ---\n")
+            if flag == 1:
+                file.write(f"{answer}\n")
+            file.write(f"{question}\n")
+    except OSError as error:
+        print(f"Помилка при роботі з файлом: {error}")
+
 filename = "python_questions.txt"
 
-student_name = "Ustymenko"
-question = "Як перевірити тип даних змінної в Python?"
-
+# Повне очищення файлу для перезапису
 try:
-    # Відкриття файлу для запису (створення, якщо не існує)
-    with open(filename, "w", encoding="utf-8") as file:
-        # Запис прізвища та питання у файл
-        file.write(f"--- {student_name} ---\n")
-        file.write(f"{question}\n")
+    with open(filename, "r+") as file:
+        file.truncate(0)
 except OSError as error:
     print(f"Помилка при роботі з файлом: {error}")
 
+flag = 0
+
+student_name = "Ustymenko"
+answer = " "
+question = "Як перевірити тип даних змінної в Python?"
+
+write_data(filename, student_name, answer, question, flag)
+
 print(f"Файл '{filename}' створено та заповнено питанням.")
+
+# Milanovych Dmytro
+flag = 1  # Для допису рядка answer (опціонально)
+
+student_name = "Milanovych"
+answer = "Потрібно використати функцію type()"
+question = "Які методи використовуються для зчитування вмісту файлу текстового формату в Python?"
+
+write_data(filename, student_name, answer, question, flag)
+
+print(f"Файл '{filename}' заповнено відповіддю та новим питанням.")
--- Лаба13/1.py
+++ Лаба13/1.py
@@ -1,7 +1,6 @@
 import csv
 import json
 
-
 def csv_to_json(csv_file_path, json_file_path):  # Ustymenko
     try:
         with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
@@ -15,6 +14,21 @@ def csv_to_json(csv_file_path, json_file_path):  # Ustymenko
     except Exception as e:
         print(f"Помилка: {e}")
 
+def json_to_csv(json_file_path, csv_file_path):  # Milanovych
+    try:
+        with open(json_file_path, 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=data[0].keys())
+            writer.writeheader()
+            for row in data:
+                writer.writerow(row)
+        print(f"Файл {json_file_path} успішно конвертовано у {csv_file_path}")
+    except FileNotFoundError:
+        print(f"Файл {json_file_path} не знайдено.")
+    except Exception as e:
+        print(f"Помилка: {e}")
+
 
 # Створення .csv файлу з кількома рядками:
 with open('data.csv', 'w', encoding='utf-8', newline='') as csvfile:  # Ustymenko
@@ -25,3 +39,22 @@ with open('data.csv', 'w', encoding='utf-8', newline='') as csvfile:  # Ustymenk
 
 # Конвертація .csv у .json:
 csv_to_json('data.csv', 'data.json')
+
+# Milanovych
+# Додавання даних до файлу .json
+
+with open('data.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)  # Завантаження вмісту файлу JSON
+
+# Додавання нового словника (рядка) до списку існуючих даних
+data.append({"Name": "Andriy", "Age": 18, "City": "Sumy"})
+data.append({"Name": "Anna", "Age": 23, "City": "Poltava"})
+
+# Збереження оновленого списку у файл JSON
+with open('data.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, indent=4)
+print(f"Дані успішно додано до файлу data.json")
+
+
+# Конвертація
+json_to_csv('data.json', 'data.csv')
